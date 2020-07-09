b93626a20b90d0310d25b644c49053dcb14e9815
RonaldNolascoU <alexnolasco_2010@hotmail.com>
2020-07-08 20:25:50 -0600

    feat(deploy): Added Firebase deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDhn1GTSyHyvYJoGyZ5k_L6_z3pT5hJKu0",
+    authDomain: "to-dolist-2772c.firebaseapp.com",
+    databaseURL: "https://to-dolist-2772c.firebaseio.com",
+    projectId: "to-dolist-2772c",
+    storageBucket: "to-dolist-2772c.appspot.com",
+    messagingSenderId: "474362929675",
+    appId: "1:474362929675:web:7f6a41e7f83773fc5d5d0e",
+    measurementId: "G-WLYWS058YG"
+  }
 };
